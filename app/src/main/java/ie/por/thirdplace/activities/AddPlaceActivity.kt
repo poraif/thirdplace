c3b2436840576078a70d0ea0cef150dbf6f2ef37
poraif <peadar.oraif@gmail.com>
2024-10-12 15:35:35 +0200

    added place data model

--- app/src/main/java/ie/por/thirdplace/activities/AddPlaceActivity.kt
+++ app/src/main/java/ie/por/thirdplace/activities/AddPlaceActivity.kt
@@ -1,17 +1,36 @@
 package ie.por.thirdplace.activities
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import ie.por.thirdplace.R
+import ie.por.thirdplace.databinding.ActivityAddplaceBinding
 import timber.log.Timber
 import timber.log.Timber.i
 
 class AddPlaceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddplaceBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_addplace)
+        binding = ActivityAddplaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAdd.setOnClickListener() {
+            val thirdPlaceTitle = binding.thirdPlaceTitle.text.toString()
+            val thirdPlaceDescription = binding.thirdPlaceDescription.text.toString()
+            val thirdPlaceType = binding.placeTypeRadioGroup.checkedRadioButtonId
+            if (thirdPlaceTitle.isNotEmpty()) {
+                i("add Button Pressed: $thirdPlaceTitle")
+            }
+            else {
+                Snackbar
+                    .make(it,"Please Enter a title", Snackbar.LENGTH_LONG)
+                    .show()
+            }
+        }
 
         Timber.plant(Timber.DebugTree())
-        i("Third Place Activity started...")
+        i("Third Place Add Place activity started...")
     }
 }
---
+++ app/src/main/java/ie/por/thirdplace/models/ThirdPlaceModel.kt
@@ -0,0 +1,3 @@
+package ie.por.thirdplace.models
+
+data class ThirdPlaceModel(var title: String = "", var description: String = "", var amenities: List<String> = listOf(), var type: String = "")

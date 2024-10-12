10184bd03679880588da7528cd30444bb22dd662
poraif <peadar.oraif@gmail.com>
2024-10-12 13:25:39 +0200

    added checkboxes and radio buttons layouts for add place

---
+++ app/src/main/java/ie/por/thirdplace/activities/PlaceCheckboxesActivity.kt
@@ -0,0 +1,10 @@
+package ie.por.thirdplace.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ie.por.thirdplace.R
+import timber.log.Timber
+import timber.log.Timber.i
+
+class PlaceCheckboxesActivity {
+}

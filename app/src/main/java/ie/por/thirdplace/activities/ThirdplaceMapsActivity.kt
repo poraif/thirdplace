486f972412658005130cf666e732425b2d114e6c
poraif <peadar.oraif@gmail.com>
2024-10-19 20:35:27 +0200

    map activity updated

--- app/src/main/java/ie/por/thirdplace/activities/ThirdplaceMapsActivity.kt
+++ app/src/main/java/ie/por/thirdplace/activities/ThirdplaceMapsActivity.kt
@@ -2,20 +2,73 @@ package ie.por.thirdplace.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import ie.por.thirdplace.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import ie.por.thirdplace.databinding.ActivityThirdplaceMapsBinding
+import ie.por.thirdplace.databinding.ContentThirdplaceMapsBinding
+import ie.por.thirdplace.main.MainApp
 
 class ThirdplaceMapsActivity : AppCompatActivity() {
 
+    lateinit var app: MainApp
     private lateinit var binding: ActivityThirdplaceMapsBinding
+    private lateinit var contentBinding: ContentThirdplaceMapsBinding
+    lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        app = application as MainApp
+
         binding = ActivityThirdplaceMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
 
+        contentBinding = ContentThirdplaceMapsBinding.bind(binding.root)
+        contentBinding.mapView.onCreate(savedInstanceState)
+
+        contentBinding.mapView.getMapAsync {
+            map = it
+            configureMap()
+        }
+
     }
+
+    private fun configureMap() {
+        map.uiSettings.isZoomControlsEnabled = true
+        app.thirdPlaces.findAll().forEach {
+            val loc = LatLng(it.lat, it.lng)
+            val options = MarkerOptions().title(it.title).position(loc)
+            map.addMarker(options)?.tag = it.id
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, it.zoom))
+        }
+    }
+
+override fun onDestroy() {
+    super.onDestroy()
+    contentBinding.mapView.onDestroy()
+}
+
+override fun onLowMemory() {
+    super.onLowMemory()
+    contentBinding.mapView.onLowMemory()
+}
+
+override fun onPause() {
+    super.onPause()
+    contentBinding.mapView.onPause()
+}
+
+override fun onResume() {
+    super.onResume()
+    contentBinding.mapView.onResume()
+}
+
+override fun onSaveInstanceState(outState: Bundle) {
+    super.onSaveInstanceState(outState)
+    contentBinding.mapView.onSaveInstanceState(outState)
+}
 }

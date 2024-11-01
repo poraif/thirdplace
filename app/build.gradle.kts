b6e0e0d2e06fea480781f22339a3725b28d7f660
poraif <peadar.oraif@gmail.com>
2024-11-01 13:35:33 +0100

    updates to UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "ie.por.thirdplace"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "ie.por.thirdplace"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
